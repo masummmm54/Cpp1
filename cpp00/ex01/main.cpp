59686df84878298933d8c9d37903a455f64d866e
Muhammet Kose <muhakose@1-C-18.42heilbronn.de>
2024-03-23 15:39:11 +0100

    masum

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,22 +1,18 @@
 #include "phonebook.hpp"
 
-class Contact
-{
-	public:
-	std::string		name;
-	std::string		last_name;
-	std::string		nick_name;
-	std::string		phone_number;
-	std::string		dark_secret;
-};
 
-class PhoneBook
+int	all_num(std::string number)
 {
-	public:
-	int			contact_num;
-	int			index;
-	Contact		*contacts;
-};
+	int i = 0;
+
+	while (number[i])
+	{
+		if (!(std::isdigit(number[i])))
+			return (0);
+		i++;
+	}
+	return(1);
+}
 
 void	add(PhoneBook *book)
 {
@@ -25,27 +21,38 @@ void	add(PhoneBook *book)
 	std::string		nick_name;
 	std::string		phone_number;
 	std::string		dark_secret;
-	std::cout << "Contact name: ";
+
 	while (name.empty())
+	{
+		std::cout << "Contact name: ";
 		std::getline(std::cin, name);
-	std::cout << "Contact last name: ";
+	}
 	while (last_name.empty())
+	{
+		std::cout << "Contact last name: ";
 		std::getline(std::cin, last_name);
-	std::cout << "Contact nick name: ";
+	}
 	while (nick_name.empty())
+	{
+		std::cout << "Contact nick name: ";
 		std::getline(std::cin, nick_name);
-	std::cout << "Contact phone number: ";
-	while (phone_number.empty())
+	}
+	while (phone_number.empty() || !all_num(phone_number))
+	{
+		std::cout << "Contact phone number: ";
 		std::getline(std::cin, phone_number);
-	std::cout << "Contact darkest secret: ";
+	}	
 	while (dark_secret.empty())
+	{
+		std::cout << "Contact darkest secret: ";
 		std::getline(std::cin, dark_secret);
+	}
 	std::cout << std::endl;
-	book->contacts[book->index].name = name;
-	book->contacts[book->index].last_name = last_name;
-	book->contacts[book->index].nick_name = nick_name;
-	book->contacts[book->index].phone_number = phone_number;
-	book->contacts[book->index].dark_secret = dark_secret;
+	book->contacts[book->index].add_name(name);
+	book->contacts[book->index].add_last_name(last_name);
+	book->contacts[book->index].add_nick_name(nick_name);
+	book->contacts[book->index].add_phone_number(phone_number);
+	book->contacts[book->index].add_dark_secret(dark_secret);
 	if (book->contact_num < 8)
 		book->contact_num++;
 	if (book->index != 7)
@@ -77,37 +84,6 @@ void	visuale_contact(std::string n)
 	}
 }
 
-void	print_contac(Contact n, int index)
-{
-	int	m = 0;
-	std::cout << "|";
-	std::cout << index + 1;
-	while (m < 9)
-	{
-		std::cout << " ";
-		m++;
-	}
-	std::cout << "|";
-	visuale_contact(n.name);
-	std::cout << "|";
-	visuale_contact(n.last_name);
-	std::cout << "|";
-	visuale_contact(n.nick_name);
-	std::cout << "|";
-	std::cout << std::endl;
-}
-
-void	show_contact(Contact n)
-{
-	std::cout << std::endl;
-	std::cout << "Name:           " << n.name << std::endl;
-	std::cout << "Last name:      " << n.last_name << std::endl;
-	std::cout << "Nick name:      " << n.nick_name << std::endl;
-	std::cout << "Phone number:   " << n.phone_number << std::endl;
-	std::cout << "Darkest secret: " << n.dark_secret << std::endl;
-	std::cout << std::endl;
-}
-
 void	search(PhoneBook *book)
 {
 	int	index = 0;
@@ -116,14 +92,14 @@ void	search(PhoneBook *book)
 	while (index < book->contact_num)
 	{
 		std::cout << "|-------------------------------------------|" << std::endl;
-		print_contac(book->contacts[index], index);
+		book->contacts[index].print_contac(index);
 		index++;
 	}
 	std::cout << " ------------------------------------------- " << std::endl;
 	std::cout << "Index number for contact: " << std::endl;
 	std::cin >> index;
 	if (index <= book->contact_num && index > 0)
-		show_contact(book->contacts[index - 1]);
+		book->contacts[index - 1].show_contact();
 	else 
 		std::cout << "Wrong index.\n" << std::endl;
 }
@@ -158,7 +134,7 @@ int	main(int ac, char **av)
 			search(&book);
 			break;
 			case 3:
-			std::cout << "EXIT command" << std::endl;
+			std::cout << "Exiting phonebook." << std::endl;
 			delete[] (book.contacts);
 			exit(0);
 			break;
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -8,7 +8,78 @@
 #include <string>
 #include <cstdlib>
 
+void	visuale_contact(std::string n);
+void	visuale_contact(std::string n);
 
+class Contact
+{
+	private:
+	std::string		name;
+	std::string		last_name;
+	std::string		nick_name;
+	std::string		phone_number;
+	std::string		dark_secret;
 
+	public:
+	void	show_contact()
+	{
+		std::cout << std::endl;
+		std::cout << "Name:           " << name << std::endl;
+		std::cout << "Last name:      " << last_name << std::endl;
+		std::cout << "Nick name:      " << nick_name << std::endl;
+		std::cout << "Phone number:   " << phone_number << std::endl;
+		std::cout << "Darkest secret: " << dark_secret << std::endl;
+		std::cout << std::endl;
+	}
+
+	void	print_contac(int index)
+	{
+		int	m = 0;
+		std::cout << "|";
+		std::cout << index + 1;
+		while (m < 9)
+		{
+			std::cout << " ";
+			m++;
+		}
+		std::cout << "|";
+		visuale_contact(name);
+		std::cout << "|";
+		visuale_contact(last_name);
+		std::cout << "|";
+		visuale_contact(nick_name);
+		std::cout << "|";
+		std::cout << std::endl;
+	}
+
+	void add_name(std::string s)
+	{
+		name = s;
+	}
+	void add_last_name(std::string s)
+	{
+		last_name = s;
+	}
+	void add_nick_name(std::string s)
+	{
+		nick_name = s;
+	}
+	void add_phone_number(std::string s)
+	{
+		phone_number = s;
+	}
+	void add_dark_secret(std::string s)
+	{
+		dark_secret = s;
+	}
+};
+
+class PhoneBook
+{
+	public:
+	int			contact_num;
+	int			index;
+	Contact		*contacts;
+};
 
 #endif
